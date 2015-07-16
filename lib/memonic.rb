369d27c16fdb43abba49a5baea51cc1ef947ab21
John Carney <john@carney.id.au>
2015-07-16 21:44:29 +1000

    Experimenting with performance improvements.

--- lib/memonic.rb
+++ lib/memonic.rb
@@ -8,17 +8,26 @@ module Memonic
   private
 
   def memoize(variable, &block)
-    if instance_variable_defined?(variable)
-      instance_variable_get(variable)
-    else
-      instance_variable_set(variable, instance_exec(&block))
+    instance_variable_get(variable) || begin
+      if instance_variable_defined?(variable)
+        nil
+      else
+        instance_variable_set(variable, instance_exec(&block))
+      end
     end
   end
 
   module ClassMethods
     def memoize(name, &block)
-      variable = "@#{name}".to_sym
-      define_method(name) { memoize(variable, &block) }
+      define_method("compute_#{name}", &block)
+      class_eval <<-RUBY
+        def #{name}
+          unless defined? @#{name}
+            @#{name} = compute_#{name}
+          end
+          @#{name}
+        end
+      RUBY
     end
   end
 end
